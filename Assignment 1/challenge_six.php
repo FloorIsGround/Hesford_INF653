a4938ca0d4ee52fb358eecab33c6a8f5c0d1d2ec
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-03 01:17:02 -0600

    Create challenge_six.php

---
+++ Assignment 1/challenge_six.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+<?php
+    $number1 = 10;
+    echo "Number 1: " . $number1 . "<br>";
+    $number2 = 5;
+    echo "Number 2: " . $number2 . "<br>";
+
+    $addition = $number1 + $number2;
+    echo "Addition: " . $addition . "<br>";
+
+    $subtraction = $number1 - $number2;
+    echo "Subtraction: " . $subtraction . "<br>";
+
+    $multiplication = $number1 * $number2;
+    echo "Multiplication: " . $multiplication . "<br>";
+    
+    $division = $number1 / $number2;
+    echo "Division: " . $division . "<br>";
+
+    $modulus = $number1  % $number2;
+    echo "Modulus: " . $modulus . "<br>";
+?>
+</body>
+</html>
