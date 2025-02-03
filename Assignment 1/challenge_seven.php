d83d68ae1fa32f1be4fa6bd459daf92c3e63d499
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-03 01:31:27 -0600

    Create challenge_seven.php

---
+++ Assignment 1/challenge_seven.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+    <?php 
+        $num = 7;
+        echo "Input: " . $num . "<br>";
+        echo ($num % 2 == 0) ? "Output: " . $num . " is an even number." : "Output: " . $num . " is an odd number."
+        
+    ?>
+</body>
+</html>
