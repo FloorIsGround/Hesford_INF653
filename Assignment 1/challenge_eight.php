30ca1103ac393d31b0d831890d79434a55cbc988
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-03 01:46:46 -0600

    Create challenge_eight.php

---
+++ Assignment 1/challenge_eight.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+    <?php 
+        $num = 10;
+        echo "Starting number: " . $num . "<br>";
+        
+        $num++;
+        echo "After increment: " . $num . "<br>";
+
+        $num--;
+        echo "After decrement: " . $num . "<br>";
+    ?>
+</body>
+</html>
