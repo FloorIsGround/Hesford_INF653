f73cdb5165d2322a2f5659f7cc70a31e61082b29
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-02 23:56:17 -0600

    Create challenge_two.php

---
+++ Assignment 1/challenge_two.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+    <?php
+        echo "\"He said, \"PHP is fun!\" and left.\"";
+        echo "\nFirst Line \nSecond Line";
+    ?>
+</body>
+</html>
