9565a1b54da5b7983447afd7cc858d51f73fb998
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-03 01:46:51 -0600

    Create challenge_ten.php

---
+++ Assignment 1/challenge_ten.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+    <?php 
+        $year = 2024;
+        echo "Input: " . $year . "<br>";
+        echo ($year % 4 == 0) ? "Output: " . $year . " is a leap year." : "Output: " . $year . " is not a leap year."
+        
+    ?>
+</body>
+</html>
