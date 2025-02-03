8dddb171144c35ac31d49d34297f041e1d2e7dfc
FloorIsGround <jrhesford@mail.fhsu.edu>
2025-02-02 23:56:13 -0600

    Create challenge_one.php

---
+++ Assignment 1/challenge_one.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+</head>
+<body>
+    <?php
+        $name = 'Jace';
+        $age = 22;
+        $favorite_color = 'light blue';
+
+        echo "My name is " . $name . ", I am " . $age . " years old and my favorite color is " . $favorite_color . ".";
+    ?>
+</body>
+</html>
